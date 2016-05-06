c4c66ba75144b42cbd188c22b86cefc857291906
Newton Jain <newton.jain@gmail.com>
2016-05-06 15:42:49 -0700

    Added correct sln and jsproj file

--- app/app.ts
+++ app/app.ts
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform: ionic.platform.IonicPlatformService) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
